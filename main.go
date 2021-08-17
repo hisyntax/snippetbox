f7a004666bdb8ed02bca0107ccabd45f3d1cc377
hisyntax <hisyntax0@gmail.com>
2021-08-18 00:04:48 +0100

    An if statement has been added to createSnippet method to check if request is POST or not

--- main.go
+++ main.go
@@ -16,6 +16,12 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
