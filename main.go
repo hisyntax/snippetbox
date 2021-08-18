0009e8dd6c94cae8e00e88b0aba14af905fc5a20
hisyntax <hisyntax0@gmail.com>
2021-08-18 02:35:16 +0100

    Added a check for the request id if it is valid

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +14,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is from the snippet"))
 }
 func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Display a specific snippet..."))
 }
 func createSnippet(w http.ResponseWriter, r *http.Request) {
