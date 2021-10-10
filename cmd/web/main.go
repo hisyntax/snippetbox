3675d86284be7280b3e701ffdc994d6f547ddfa3
hisyntax <hisyntax0@gmail.com>
2021-10-10 22:05:48 +0100

    Css, Js and Images is being served

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,10 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+	//To serve all the static files our application depends on to run
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :8000")
 	err := http.ListenAndServe(":8000", mux)
 	if err != nil {
